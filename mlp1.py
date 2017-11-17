4a6ab58f49e446e797b072d6cdce3bbf8157174d
Tomer Gill <coolran103@gmail.com>
2017-11-17 19:23:29 +0200

    advancing MLP1 loss and gradients

--- mlp1.py
+++ mlp1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 STUDENT = {'name': 'Tomer Gill',
            'ID': '318459450'}
@@ -25,8 +26,27 @@ def predict(x, params):
 
 def loss_and_gradients(x, y, params):
     # YOU CODE HERE
+    W1, b1, W2, b2 = params
     probs = classifier_output(x, params)
-    return ...
+    loss = -1 * math.log(probs[y])
+
+    yvector = np.zeros_like(x)
+    yvector[y] = 1
+
+    def gtag(v):
+        return 1 - tanh(v) ** 2
+
+    #gWs
+    a = [x]
+    a.append(tanh(a[0].dot(W1)))
+    a.append(tanh(a[1].dot(W2)))
+    lower_delta = [a[2] - yvector]
+    lower_delta.append((W1.dot(lower_delta[0])) * gtag(a[1]))
+    lower_delta.reverse()
+
+    delta = [a_i.dot(lower_delta[i]) for i, a_i in enumerate(a[1:])]
+
+    return [loss, gb, delta]
 
 def create_classifier(in_dim, hid_dim, out_dim):
     """
@@ -34,10 +54,10 @@ def create_classifier(in_dim, hid_dim, out_dim):
     with input dimension in_dim, hidden dimension hid_dim,
     and output dimension out_dim.
     """
-    W1 = np.zeros((in_dim, hid_dim))
-    b1 = np.zeros(hid_dim)
-    W2 = np.zeros((hid_dim, out_dim))
-    b2 = np.zeros(out_dim)
+    W1 = np.random.rand(in_dim, hid_dim)
+    b1 = np.random.rand(hid_dim)
+    W2 = np.random.rand(hid_dim, out_dim)
+    b2 = np.random.rand(out_dim)
     params = [W1,b1,W2,b2]
     return params
 
